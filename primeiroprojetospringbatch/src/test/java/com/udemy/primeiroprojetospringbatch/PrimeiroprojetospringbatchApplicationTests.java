a223b78909a1d159690c7b6ef741e4013fad1438
Kauê Oliveira <kso13@hotmail.com>
2023-06-04 07:29:25 -0300

    Seção 2 - Visão geral Spring Batch. Criação do projeto e Hello world

---
+++ primeiroprojetospringbatch/src/test/java/com/udemy/primeiroprojetospringbatch/PrimeiroprojetospringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.primeiroprojetospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroprojetospringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
