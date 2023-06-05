51f43bbee9a509e271967ea0be2502e53e123e6b
Kauê Oliveira <kso13@hotmail.com>
2023-06-05 03:46:52 -0300

    Seção 3 - Metadados com MySql, multiplas execuções, escopo e argumentos

--- primeiroprojetospringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
+++ primeiroprojetospringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
@@ -37,12 +37,9 @@ public class BatchConfig {
     @Bean
     @StepScope
     public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
-        return new Tasklet() {
-            @Override
-            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                System.out.printf("Olá, %s!%n", nome);
-                return RepeatStatus.FINISHED;
-            }
+        return (contribution, chunkContext) -> {
+            System.out.printf("Olá, %s!%n", nome);
+            return RepeatStatus.FINISHED;
         };
     }
 }
