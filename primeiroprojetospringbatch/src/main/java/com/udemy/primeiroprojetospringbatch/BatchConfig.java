d0feb0b557faafe5651eb27279058ef36f395700
Kauê Oliveira <kso13@hotmail.com>
2023-06-05 03:44:10 -0300

    Seção 3 - Metadados com MySql, multiplas execuções, escopo e argumentos

--- primeiroprojetospringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
+++ primeiroprojetospringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
@@ -2,10 +2,16 @@ package com.udemy.primeiroprojetospringbatch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -14,19 +20,29 @@ import org.springframework.transaction.PlatformTransactionManager;
 public class BatchConfig {
 
     @Bean
-    public Job job(JobRepository jobRepository, Step step) {
+    public Job imprimeOlaJob(JobRepository jobRepository, Step step) {
         return new JobBuilder("job", jobRepository)
                 .start(step)
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
-    public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Step imprimeOlaStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("step", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Olá mundo!");
-                    return RepeatStatus.FINISHED;
-                }, transactionManager)
+                .tasklet(imprimeOlaTasklet("pipoca"), transactionManager)
                 .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet imprimeOlaTasklet(@Value("#{jobParameters['nome']}") String nome) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.printf("Olá, %s!%n", nome);
+                return RepeatStatus.FINISHED;
+            }
+        };
     }
 }
