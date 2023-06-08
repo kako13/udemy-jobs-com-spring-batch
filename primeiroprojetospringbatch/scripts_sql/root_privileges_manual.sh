69109e4751a24a18fe567c93d3c5b011edd95466
Kauê Oliveira <kso13@hotmail.com>
2023-06-08 08:37:22 -0300

    Seção 3 - Renomeando projeto

--- primeiroprojetospringbatch/scripts_sql/root_privileges_manual.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-until mysql -u root -proot -e "update mysql.user set host='%' where user='root'; flush privileges;"; do
-    echo "Aguardando o MySQL iniciar..."
-    sleep 1
-done
-
-echo -e "\n>>>PRIVILÉGIOS CONCEDIDOS AO USER ROOT DURANTE A INICIALIZAÇÃO<<<"
