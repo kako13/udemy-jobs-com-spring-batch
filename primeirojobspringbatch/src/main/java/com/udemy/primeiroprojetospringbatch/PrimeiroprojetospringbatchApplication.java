40cf03623d288d3921a5c5a628418bce546b0d5a
Kauê Oliveira <kso13@hotmail.com>
2023-06-08 08:41:49 -0300

    Seção 3 - Renomeando projeto

---
+++ primeirojobspringbatch/src/main/java/com/udemy/primeiroprojetospringbatch/PrimeiroprojetospringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.primeiroprojetospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroprojetospringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroprojetospringbatchApplication.class, args);
+	}
+
+}
