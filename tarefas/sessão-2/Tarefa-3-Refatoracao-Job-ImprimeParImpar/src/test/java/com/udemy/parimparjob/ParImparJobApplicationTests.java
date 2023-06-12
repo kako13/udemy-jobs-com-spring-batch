5364dea79c418702392492bb979fc6a8f8030aea
Kauê Oliveira <kso13@hotmail.com>
2023-06-12 12:26:58 -0300

    Seção 2 - Tarefa 3 - Refatorando imprimeParImparAlonsoJob, separando classes por finalidade

---
+++ tarefas/sessão-2/Tarefa-3-Refatoracao-Job-ImprimeParImpar/src/test/java/com/udemy/parimparjob/ParImparJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.parimparjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParImparJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
