63a9dbfab2005a7c1cf545e4d58b87f82f6adbdd
Kauê Oliveira <kso13@hotmail.com>
2023-06-27 06:39:32 -0300

    Seção 5 - Reestar de Job na leitura de arquivo

--- arquivo-largura-fixa-job/src/main/java/com/springbatch/arquivolargurafixa/writer/LeituraArquivoLarguraFixaWriterConfig.java
+++ arquivo-largura-fixa-job/src/main/java/com/springbatch/arquivolargurafixa/writer/LeituraArquivoLarguraFixaWriterConfig.java
@@ -4,12 +4,24 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.springbatch.arquivolargurafixa.dominio.Cliente;
-
 @Configuration
 public class LeituraArquivoLarguraFixaWriterConfig {
 	@Bean
 	public ItemWriter<? super Object> leituraArquivoLarguraFixaWriter() {
+//		TODO: Para execução normal, descomente o código abaixo:
 		return items -> items.forEach(System.out::println);
+
+//	TODO: Para estimular interrupção de execução, descomente o código abaixo:
+//	return items -> {
+//		for (Object item : items) {
+//
+//			Cliente cliente = (Cliente) item;
+//
+//			if (cliente.getNome().equals("Maria"))
+//				throw new Exception("Erro ao processar o elemento: " + cliente);
+//			else
+//				System.out.println("Cliente ignorado: " + cliente);
+//		}
+//	};
 	}
 }
